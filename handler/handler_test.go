7fc9c26088a34e89cee8f3a8bbc34c4cc696e3d0
Jimmy <jtruong2@comcast.net>
2021-06-16 13:44:40 -0600

    Added `handler` package
    This package is responsible for calling the `Asker` and subsequently
    calling the `AverageMinutes` function with the user-entered finishTimes

---
+++ handler/handler_test.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"io"
+	"testing"
+	"errors"
+)
+
+type mockAskService struct {
+	finishTimeInput string
+	addMoreInput string
+}
+
+type mockCalculatorService struct {
+	err error
+}
+
+func TestHandleInputs(t *testing.T) {
+	cases := []struct{
+		name string
+		mas *mockAskService
+		mcs *mockCalculatorService
+	}{
+		{
+			name: "when addMore equals no",
+			mas: &mockAskService{
+				finishTimeInput: "12:00 PM, DAY 1",
+				addMoreInput: "n",
+			},
+			mcs: &mockCalculatorService{
+				err: nil,
+			},
+		},
+		{
+			name: "when error in AverageMinutes",
+			mas: &mockAskService{
+				finishTimeInput: "12:00 PM, DAY 1",
+				addMoreInput: "n",
+			},
+			mcs: &mockCalculatorService{
+				err: errors.New("failed"),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			HandleInputs(tc.mas, tc.mcs)
+		})
+	}
+}
+
+func (mas *mockAskService) AskForFinishTimes(stdin io.Reader, finishTime *string) {
+	*finishTime = mas.finishTimeInput
+}
+
+func (mas *mockAskService) AskToAddMoreTimes(stdin io.Reader, addMore *string) {
+	*addMore = mas.addMoreInput
+}
+
+func (mcs *mockCalculatorService) AverageMinutes(startDate string, finishTimes []string) (int, error) {
+	return 0, mcs.err
+}
